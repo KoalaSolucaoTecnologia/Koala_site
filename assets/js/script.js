552ed514b3908c991975f4058199645257f9f515
ProjetosBrunoCesar <brunocesarsambarock@gmail.com>
2018-09-28 14:51:40 -0300

    Alteracao carousel e script.js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function carousel() {
+    var myIndex = 0;
+    var i = 0;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+        myIndex++;
+    }
+
+
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+}
+
+
+carousel();
